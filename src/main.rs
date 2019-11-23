40cf7219e1a83f18bf22e42de75be0f435abfafe
Matt Traudt <sirmatt@ksu.edu>
2019-11-23 15:03:47 -0600

    Take a stab at cli args, (de)serialize DieWeights/RollWeights

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use cdc2::global::conf_def;
 use cdc2::randroll::DieWeights;
 use cdc2::roll::Roll;
 use cdc2::table::{BankrollRecorder, PassPlayer, Player, Table};
-use clap::{App, Arg, SubCommand, crate_version, crate_name};
+use clap::{crate_name, crate_version, App, Arg, ArgMatches, SubCommand};
 use rayon::prelude::*;
-use std::io::{self, Read};
+use std::fs::OpenOptions;
+use std::io::{self, Read, Write};
 
 struct RollReader<R>
 where
@@ -63,54 +64,150 @@ where
     (d1, d2)
 }
 
+fn roll_weights_from_roll_iter<I>(rolls: I) -> [u64; 11]
+where
+    I: Iterator<Item = Roll>,
+{
+    let mut d = [0; 11];
+    for r in rolls {
+        d[r.value() as usize - 2] += 1;
+    }
+    d
+}
+
+fn simulate(args: &ArgMatches) -> Result<(), ()> {
+    Err(())
+}
+
+fn parse_rolls(args: &ArgMatches) -> Result<(), ()> {
+    // unwrap ok: clap should have complained
+    let in_fname = args.value_of("input").unwrap();
+    let out_fname = args.value_of("output").unwrap();
+    // Open in file, exit early if can't
+    let in_fd = match OpenOptions::new().read(true).open(in_fname) {
+        Err(e) => {
+            eprintln!("Error opening input file {}: {}", in_fname, e);
+            return Err(());
+        }
+        Ok(fd) => fd,
+    };
+    // Open out file, exit early if can't
+    let mut out_fd = match OpenOptions::new().write(true).open(out_fname) {
+        Err(e) => {
+            eprintln!("Error opening output file {}: {}", out_fname, e);
+            return Err(());
+        }
+        Ok(fd) => fd,
+    };
+    // iterator over all the rolls parsed from the in file
+    let rolls = RollReader::new(in_fd);
+    // unwrap ok: clap should have complained
+    let outfmt = args.value_of("outfmt").unwrap();
+    // Based on what the desired out format is, parse the input into a single Result<String>
+    let s = if outfmt == "dieweights" {
+        let (d1, d2) = die_weights_from_roll_iter(rolls);
+        let d = DieWeights::new_weights2(d1, d2);
+        serde_json::to_string(&d)
+    } else if outfmt == "rollweights" {
+        let d = roll_weights_from_roll_iter(rolls);
+        serde_json::to_string(&d)
+    } else {
+        unimplemented!();
+    };
+    // Get String out, or return early
+    let s = match s {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("Unable to serialize {}: {}", outfmt, e);
+            return Err(());
+        }
+    };
+    // Write string to out file, or log error
+    match out_fd.write_all(s.as_bytes()) {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            eprintln!("Error writing serlized data to {}: {}", out_fname, e);
+            Err(())
+        }
+    }
+}
+
 fn main() {
-    let matches = App::new(crate_name!())
+    let args = App::new(crate_name!())
         .version(crate_version!())
         .arg(
             Arg::with_name("config")
                 .short("c")
                 .long("config")
                 .value_name("FILE")
-                .help("Specify configuration file")
-                .takes_value(true),
+                .default_value(conf_def::CONFIG)
+                .global(true),
         )
-        .subcommand(SubCommand::with_name("sim")
-                    .about("Run craps game simulations"))
-        .subcommand(SubCommand::with_name("foo")
-                    .about("Do something else"))
+        .subcommand(SubCommand::with_name("simulate").about("Run craps game simulations"))
+        .subcommand(
+            SubCommand::with_name("parserolls")
+                .about("Input rolls and output a parsed format for use with other commands")
+                .arg(
+                    Arg::with_name("input")
+                        .short("i")
+                        .long("input")
+                        .default_value("/dev/stdin"),
+                )
+                .arg(
+                    Arg::with_name("output")
+                        .short("o")
+                        .long("output")
+                        .default_value("/dev/stdout"),
+                )
+                .arg(
+                    Arg::with_name("outfmt")
+                        .long("outfmt")
+                        .possible_values(&["dieweights", "rollweights"])
+                        .default_value("rollweights"),
+                ),
+        )
         .get_matches();
-    let config = matches.value_of("config").unwrap_or(conf_def::CONFIG);
-    eprintln!("The config is {}", config);
-    if let Some(matches) = matches.subcommand_matches("sim") {
-        eprintln!("Will do sim");
-    } else if let Some(matches) = matches.subcommand_matches("foo") {
-        eprintln!("Will do foo");
+    let _config = args.value_of("config").unwrap();
+    let res = if let Some(args) = args.subcommand_matches("simulate") {
+        simulate(args)
+    } else if let Some(args) = args.subcommand_matches("parserolls") {
+        parse_rolls(args)
+    } else if args.subcommand_name().is_none() {
+        eprintln!("Must provide subcommand");
+        Err(())
+    } else {
+        eprintln!("Unknown subcommand {}", args.subcommand_name().unwrap());
+        Err(())
+    };
+    match res {
+        Err(_) => {}
+        Ok(_) => {}
     }
-    let (d1, d2) = die_weights_from_roll_iter(RollReader::new(io::stdin()));
-    //let outputs: Vec<String> = (0..10000).into_par_iter().map(|_| {
-    let outputs: Vec<String> = (0..100)
-        .into_par_iter()
-        .map(|_| {
-            let mut output = String::new();
-            let roll_gen = DieWeights::new_weights2(d1, d2);
-            let mut table = Table::new(Box::new(roll_gen));
-            let mut p = PassPlayer::new(500000);
-            p.attach_recorder(Box::new(BankrollRecorder::new()));
-            table.add_player(Box::new(p));
-            for _ in 0..1000 {
-                let finished_players = table.loop_once();
-                for p in finished_players {
-                    output += p.recorder_output();
-                }
-            }
-            let finished_players = table.done();
-            for p in finished_players {
-                output += p.recorder_output();
-            }
-            output
-        })
-        .collect();
-    for o in outputs {
-        println!("{}", o);
-    }
+    //let (d1, d2) = die_weights_from_roll_iter(RollReader::new(io::stdin()));
+    ////let outputs: Vec<String> = (0..10000).into_par_iter().map(|_| {
+    //let outputs: Vec<String> = (0..100)
+    //    .into_par_iter()
+    //    .map(|_| {
+    //        let mut output = String::new();
+    //        let roll_gen = DieWeights::new_weights2(d1, d2);
+    //        let mut table = Table::new(Box::new(roll_gen));
+    //        let mut p = PassPlayer::new(500000);
+    //        p.attach_recorder(Box::new(BankrollRecorder::new()));
+    //        table.add_player(Box::new(p));
+    //        for _ in 0..1000 {
+    //            let finished_players = table.loop_once();
+    //            for p in finished_players {
+    //                output += p.recorder_output();
+    //            }
+    //        }
+    //        let finished_players = table.done();
+    //        for p in finished_players {
+    //            output += p.recorder_output();
+    //        }
+    //        output
+    //    })
+    //    .collect();
+    //for o in outputs {
+    //    println!("{}", o);
+    //}
 }
--- src/randroll.rs
+++ src/randroll.rs
@@ -2,6 +2,8 @@ use crate::roll::Roll;
 use rand::distributions::weighted::alias_method::WeightedIndex;
 use rand::distributions::Distribution;
 use rand::{thread_rng, Rng};
+use serde::de::{Deserialize, Deserializer};
+use serde::ser::{Serialize, SerializeSeq, Serializer};
 
 pub trait RollGen {
     fn gen(&self) -> Roll;
@@ -23,8 +25,50 @@ impl_iterator!(RollWeights);
 
 #[derive(Debug)]
 pub struct DieWeights {
-    d1: WeightedIndex<u64>,
-    d2: WeightedIndex<u64>,
+    // used for generating random rolls and derived from the given weights
+    dist1: WeightedIndex<u64>,
+    dist2: WeightedIndex<u64>,
+    // what the user actually provided and what we serialize to/from
+    given1: [u64; 6],
+    given2: [u64; 6],
+}
+
+impl Serialize for DieWeights {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut seq = serializer.serialize_seq(Some(self.given1.len() + self.given2.len()))?;
+        for e in self.given1.iter() {
+            seq.serialize_element(e)?;
+        }
+        for e in self.given2.iter() {
+            seq.serialize_element(e)?;
+        }
+        seq.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for DieWeights {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let mut v1: Vec<u64> = Vec::deserialize(deserializer)?;
+        assert_eq!(v1.len(), 6 + 6);
+        let v2 = v1.split_off(6);
+        let mut w1 = [0; 6];
+        let mut w2 = [0; 6];
+        assert_eq!(v1.len(), 6);
+        assert_eq!(v2.len(), 6);
+        for (i, val) in v1.iter().enumerate() {
+            w1[i] = *val;
+        }
+        for (i, val) in v2.iter().enumerate() {
+            w2[i] = *val;
+        }
+        Ok(DieWeights::new_weights2(w1, w2))
+    }
 }
 
 impl Default for DieWeights {
@@ -41,17 +85,24 @@ impl DieWeights {
     }
 
     pub fn new_weights(w: [u64; 6]) -> Self {
-        let w = WeightedIndex::new(w.to_vec()).unwrap();
-        DieWeights {
-            d1: w.clone(),
-            d2: w,
+        let dist = WeightedIndex::new(w.to_vec()).unwrap();
+        Self {
+            dist1: dist.clone(),
+            dist2: dist,
+            given1: w.clone(),
+            given2: w,
         }
     }
 
     pub fn new_weights2(w1: [u64; 6], w2: [u64; 6]) -> Self {
-        let w1 = WeightedIndex::new(w1.to_vec()).unwrap();
-        let w2 = WeightedIndex::new(w2.to_vec()).unwrap();
-        DieWeights { d1: w1, d2: w2 }
+        let dist1 = WeightedIndex::new(w1.to_vec()).unwrap();
+        let dist2 = WeightedIndex::new(w2.to_vec()).unwrap();
+        DieWeights {
+            dist1,
+            dist2,
+            given1: w1,
+            given2: w2,
+        }
     }
 }
 
@@ -59,15 +110,44 @@ impl RollGen for DieWeights {
     fn gen(&self) -> Roll {
         let mut rng = thread_rng();
         let v = [1, 2, 3, 4, 5, 6];
-        let d1 = v[self.d1.sample(&mut rng)];
-        let d2 = v[self.d2.sample(&mut rng)];
+        let d1 = v[self.dist1.sample(&mut rng)];
+        let d2 = v[self.dist2.sample(&mut rng)];
         Roll::new([d1, d2]).unwrap()
     }
 }
 
 #[derive(Debug)]
 pub struct RollWeights {
-    d: WeightedIndex<u64>,
+    dist: WeightedIndex<u64>,
+    given: [u64; 11],
+}
+
+impl Serialize for RollWeights {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut seq = serializer.serialize_seq(Some(self.given.len()))?;
+        for e in self.given.iter() {
+            seq.serialize_element(e)?;
+        }
+        seq.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for RollWeights {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let mut w = [0; 11];
+        let mut v: Vec<u64> = Vec::deserialize(deserializer)?;
+        assert_eq!(v.len(), 11);
+        for (i, val) in v.iter().enumerate() {
+            w[i] = *val;
+        }
+        Ok(RollWeights::new_weights(w))
+    }
 }
 
 impl Default for RollWeights {
@@ -84,8 +164,8 @@ impl RollWeights {
     }
 
     pub fn new_weights(w: [u64; 11]) -> Self {
-        let d = WeightedIndex::new(w.to_vec()).unwrap();
-        RollWeights { d }
+        let dist = WeightedIndex::new(w.to_vec()).unwrap();
+        RollWeights { dist, given: w }
     }
 }
 
@@ -93,7 +173,7 @@ impl RollGen for RollWeights {
     fn gen(&self) -> Roll {
         let mut rng = thread_rng();
         let v = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-        let v = v[self.d.sample(&mut rng)];
+        let v = v[self.dist.sample(&mut rng)];
         // pick a random value for the first die, which will determine the second die value too
         let d1 = if v <= 7 {
             rng.gen_range(1, v)
